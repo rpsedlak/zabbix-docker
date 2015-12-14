4fd9016c65c1c6050cf139a03fdbc4af0720509d
Richard Sedlak <richard.s@gvocom.com>
2015-12-13 21:51:57 -0600

    Definition of values to collect Docker instance values.

---
+++ zabbix-docker-value.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+#################################################################
+#
+# zabbix-docker-value.py
+#
+#   A program that parses the "docker inspect" values for
+#   reporting data to Zabbix.
+#
+# Version: 1.0
+#
+# Author: Richard Sedlak
+#
+#################################################################
+
