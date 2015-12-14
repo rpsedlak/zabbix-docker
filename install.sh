3918d7ec0116311394600c51342b6730a0bce183
Richard Sedlak <richard.sedlak@mac.com>
2015-12-14 16:38:07 -0600

    Feature complete with respect to "docker info" key processing.

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# argument processing
+
+CONFIG_PATH=/etc/zabbix/zabbix_agentd.d/
+
+if [ "$#" -eq 1]; 
+	then CONFIG_PATH=$1
+fi
+
+# Copy the files
+cp -f *.py /usr/local/bin
+cp -f *.conf $CONFIG_PATH
+cp -f *.xml /tmp
+
+# tell the user some stuff
+echo "Python scripts copied to /usr/local/bin"
+echo "zabbix-agent configuration files copied to $CONFIG_PATH"
+echo "XML Zabbix Templates copied to /tmp"
+echo ""
+echo "You will need to restart the zabbix-agent and import the XML template on your Zabbix server"
+
